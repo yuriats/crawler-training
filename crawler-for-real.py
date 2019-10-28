428e64043306a342e45c3a150860e8377128ca09
Yuri Anjos-Travassos <yuri.anjos.t@gmail.com>
2019-10-28 19:07:19 -0300

    Update crawler limited

--- crawler-for-real.py
+++ crawler-for-real.py
@@ -9,19 +9,18 @@ import html5lib
 html = requests.get('http://www.healthdata.org/').text
 soup = BeautifulSoup(html, 'lxml')
 
-g_data = soup.find_all("div", {"class": "view view-recent-research-articles-and-policy-reports view-id-recent_research_articles_and_policy_reports view-display-id-block view-dom-id-34bec593b75df59bb7b7bef3e707e730"})
+for i in range(3):
+    titulo = soup.find_all("div", {"class": "views-field views-field-title"})
+    resumo = soup.find_all("div", {"class": "views-field views-field-body"})
+    data = soup.find_all("div", {"class": "views-field views-field-field-publication-date"})
+    print("Data: %s" % data[i].text)
+    print("Título: %s" % titulo[i].text)
+    print("Resumo: %s" % resumo[i].text)
+    print()
 
-for item in g_data:
-    print(item.text)
 
-
-for article in soup.find_all('span'):
-    titulo = article.find('div', class_='views-field views-field-title').a.text
-    print(titulo)
-
-    resumo = article.find('div', class_='views-field views-field-body').p.text
-    print(resumo)
-
-    data = article.find('div', class_='views-field views-field-field-publication-date').span.text
-    print(data)
-
+#not working
+#g_data = soup.find_all("div", {"class": "view view-recent-research-articles-and-policy-reports view-id-recent_research_articles_and_policy_reports view-display-id-block view-dom-id-34bec593b75df59bb7b7bef3e707e730"})
+#len(g_data)
+#for item in g_data:
+#    print(item.text)
